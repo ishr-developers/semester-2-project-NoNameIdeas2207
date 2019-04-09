9087f990694534661931f1960561c08965be8e02
tony-albanese <tony.albanese2000@gmail.com>
2019-04-09 12:11:39 +0200

    Added TODOs.

--- app/src/main/java/e/santiago/steamsaleapp/MainActivity.java
+++ app/src/main/java/e/santiago/steamsaleapp/MainActivity.java
@@ -1,14 +1,21 @@
 package e.santiago.steamsaleapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+//TODO: This Activity will implement LoaderManager.LoaderCallbacs() interface so it can retrieve data from the internet.
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
+
+        downloadData();
+    }
+
+    public void downloadData() {
+
+        //TODO: Either you need a complete url or you need to build one from the user data.
     }
 }
