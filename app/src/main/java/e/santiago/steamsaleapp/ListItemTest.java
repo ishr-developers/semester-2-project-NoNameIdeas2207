d2bf5cbecd6e60088d60def740c63763c6d59332
tony-albanese <tony.albanese2000@gmail.com>
2019-04-09 12:07:55 +0200

    Added a comment.

--- app/src/main/java/e/santiago/steamsaleapp/ListItemTest.java
+++ app/src/main/java/e/santiago/steamsaleapp/ListItemTest.java
@@ -1,13 +1,16 @@
 package e.santiago.steamsaleapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+//This activity name makes no sense. Refactor it so that the name describes what the Activity does.
 public class ListItemTest extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_item_test);
+
+
     }
 }
