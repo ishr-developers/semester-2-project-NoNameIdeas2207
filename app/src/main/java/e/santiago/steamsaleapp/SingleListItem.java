905a388111aafb1d5d4b66307d464a50037a107b
NoNameIdeas2207 <santiago-romero@is-hr.de>
2019-04-09 14:57:57 +0200

    Did a bit more on the single list item, added the search bar edit text and created the activity for when you select a game, however none of this has actual functionality yet

--- app/src/main/java/e/santiago/steamsaleapp/SingleListItem.java
+++ app/src/main/java/e/santiago/steamsaleapp/SingleListItem.java
@@ -3,13 +3,13 @@ package e.santiago.steamsaleapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-//This activity name makes no sense. Refactor it so that the name describes what the Activity does.
-public class ListItemTest extends AppCompatActivity {
+
+public class SingleListItem extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_item_test);
+        setContentView(R.layout.single_list_item);
 
 
     }
